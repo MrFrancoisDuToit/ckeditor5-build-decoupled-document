6fc487b8becb015b94977eaa611304a9e72bc825
vagrant <vagrant@homestead>
2020-01-13 10:18:00 -0800

    Add SimpleUploadAdapter

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -35,6 +35,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
 export default class DecoupledEditor extends DecoupledEditorBase {}
 
@@ -68,7 +69,8 @@ DecoupledEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	SimpleUploadAdapter,
 ];
 
 // Editor configuration.
